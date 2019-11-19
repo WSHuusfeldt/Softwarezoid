1525d8de7f657ed2eea91210f187bbbf90a5de33
Asger H. Sørensen <znaszger@hotmail.com>
2019-11-19 18:37:05 +0100

    Contact frontend done

--- Frontend/src/App.js
+++ Frontend/src/App.js
@@ -10,6 +10,7 @@ import LoginForm from './login/LoginForm';
 import Data from './pages/Data';
 import ProductDetails from "./pages/ProductDetails";
 import FAQ from './pages/FAQ';
+import Contact from "./pages/Contact";
 
 // Animations
 import { FadeInLeft, FadeInUp, Bounce } from './style/animations'
@@ -18,12 +19,12 @@ import { FadeInLeft, FadeInUp, Bounce } from './style/animations'
 function App() {
   return (
     <div className="App">
-        <Switch>
-          {/* NO MORE ROUTES HERE!!!!  */}
-          <Route exact path={URLSettings.getURL("Home")}> <Welcome /> </Route>
-          <Route path={URLSettings.getURL("NoMatch")}> <Header /> </Route>
-        </Switch>
-        <main>
+      <Switch>
+        {/* NO MORE ROUTES HERE!!!!  */}
+        <Route exact path={URLSettings.getURL("Home")}> <Welcome /> </Route>
+        <Route path={URLSettings.getURL("NoMatch")}> <Header /> </Route>
+      </Switch>
+      <main>
         <div className="container-fluid py-3 mt-5 pt-5 pb-5">
           <Switch>
             {/* NEW ROUTES GOES HERE!  */}
@@ -31,11 +32,12 @@ function App() {
             <Route path={URLSettings.getURL("Data")}> <Data /> </Route>
             <Route path={URLSettings.getURL("FAQ")}> <FAQ /> </Route>
             <Route path={URLSettings.getURL("ProductId")}> <ProductDetails /> </Route>
+            <Route path={URLSettings.getURL("Contact")}> <Contact /> </Route>
             <Route path={URLSettings.getURL("NoMatch")}> <NoMatch /> </Route>
           </Switch>
-          </div>
-        </main>
-        <Footer />
+        </div>
+      </main>
+      <Footer />
     </div>
   )
 }
