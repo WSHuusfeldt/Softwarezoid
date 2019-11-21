65e65734d1385ec81f432f77672a8b9908b66e17
Andreas Vikke <vikkedesign@gmail.com>
2019-11-21 11:48:43 +0100

    Swagger

--- Backend/src/main/java/rest/ApplicationConfig.java
+++ Backend/src/main/java/rest/ApplicationConfig.java
@@ -10,6 +10,7 @@ public class ApplicationConfig extends Application {
     public Set<Class<?>> getClasses() {
         Set<Class<?>> resources = new java.util.HashSet<>();
         addRestResourceClasses(resources);
+        
         return resources;
     }
 
