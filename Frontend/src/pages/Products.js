933f300b312a169fb5999ea3b78b20fe80ac37a0
Asger H. Sørensen <znaszger@hotmail.com>
2019-11-21 20:34:18 +0100

    Update Products.js

--- Frontend/src/pages/Products.js
+++ Frontend/src/pages/Products.js
@@ -1,5 +1,7 @@
 import React, { useState, useEffect } from 'react';
+import { Link } from 'react-router-dom';
 import Facade from '../login/ApiFacade';
+import settings from '../settings';
 
 export default function Products() {
   const [data, setData] = useState([]);
@@ -8,24 +10,25 @@ export default function Products() {
     Facade.fetchData().then(res => setData(res));
   }, [])
 
+
   return (
     <div className="container products">
-    <div className="row">
-          {data.map((software, index) => 
+      <div className="row">
+        {data.map((software, index) =>
           <div key={index} className="col-md-3">
             <div className="card">
               <figure className="text-center">
-                <img src={software.thumbnail} className="img-fluid" alt={software.title}/>
+                <img src={software.thumbnail} className="img-fluid" alt={software.title} />
               </figure>
               <div className="card-body">
                 <h5 className="card-title">{software.title}</h5>
-                <a href="product-details.html" className="btn-zoid">Show Details</a>
-                <span className="price"><b>{software.price/100},-</b></span>
+                <Link to={settings.getURL("ProductId") + "/" + (index + 1)} className="btn-zoid">Show details</Link>
+                <span className="price"><b>{software.price / 100},-</b></span>
               </div>
             </div>
           </div>
-          )}
-          </div>
-    </div>
+        )}
+      </div>
+    </div >
   )
 }
