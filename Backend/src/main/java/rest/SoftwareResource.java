32eb8fa443b3787c2c4691279eaaac23654f0d27
Asger H. Sørensen <znaszger@hotmail.com>
2019-11-21 15:12:11 +0100

    Update SoftwareResource.java

--- Backend/src/main/java/rest/SoftwareResource.java
+++ Backend/src/main/java/rest/SoftwareResource.java
@@ -52,21 +52,19 @@ public class SoftwareResource {
     public String setupDatabase() {
         EntityManager em = EMF.createEntityManager();
         List<Software> softwares = new ArrayList();
-        softwares.add(new Software("Netbeans", "Programmers dream", 280000, "https://upload.wikimedia.org/wikipedia/commons/thumb/9/98/Apache_NetBeans_Logo.svg/1200px-Apache_NetBeans_Logo.svg.png"));
-        softwares.add(new Software("Visual Studio Code", "Programmers dream", 280000, "https://mospaw.com/wp-content/uploads/2018/07/Visual_Studio_code_logo.png"));
-        softwares.add(new Software("Notepad++", "Programmers real real dream", 1000, "https://www.slashcoding.com/wp-content/uploads/2013/08/Notepad_plus_plus.png"));
-        softwares.add(new Software("Word", "Program for words", 10000, "https://d1yjjnpx0p53s8.cloudfront.net/styles/logo-thumbnail/s3/072015/word_0.png?itok=YCX08sq9"));
-        softwares.add(new Software("Excel", "Program for numbers", 10000, "https://teaching.cambriancollege.ca/wp-content/uploads/2018/07/Top-of-Page-Images.jpg"));
-        softwares.add(new Software("PowerPoint", "Program for presentation", 100000, "https://www.tcd.ie/CAPSL/TIC/assets/img/accessible-info/powerpoint.png"));
-        softwares.add(new Software("WinRar", "Program for rar files", 0, "https://cdn.worldvectorlogo.com/logos/winrar-1.svg"));
-        //softwares.add(new Software("Not real program", "Test program for very very very very very very very very very very very very very very very very very very very very very very very very long description", 0, "https://cdn3.vectorstock.com/i/1000x1000/19/77/isolated-abstract-blue-color-thumb-up-contour-logo-vector-10861977.jpg"));
-        
         softwares.add(new Software("Netbeans", "Programmers dream", 280000, "https://upload.wikimedia.org/wikipedia/commons/thumb/9/98/Apache_NetBeans_Logo.svg/1200px-Apache_NetBeans_Logo.svg.png", 
                 (Arrays.asList("Version: 14.0.4", "Compatability: Windows, MacOS, Linux"))));
         softwares.add(new Software("Visual Studio Code", "Programmers dream", 280000, "https://mospaw.com/wp-content/uploads/2018/07/Visual_Studio_code_logo.png", 
                 (Arrays.asList("Version: 14.0.4", "Compatability: Windows, MacOS, Linux"))));
         softwares.add(new Software("Notepad++", "Programmers real real dream", 1000, "https://www.slashcoding.com/wp-content/uploads/2013/08/Notepad_plus_plus.png", 
                 (Arrays.asList("Version: 14.0.4", "Compatability: Windows, MacOS, Linux"))));
+        softwares.add(new Software("Word", "Program for words", 10000, "https://d1yjjnpx0p53s8.cloudfront.net/styles/logo-thumbnail/s3/072015/word_0.png?itok=YCX08sq9", (Arrays.asList("Version: 14.0.4", "Compatability: Windows, MacOS, Linux"))));
+        softwares.add(new Software("Excel", "Program for numbers", 10000, "https://teaching.cambriancollege.ca/wp-content/uploads/2018/07/Top-of-Page-Images.jpg", (Arrays.asList("Version: 14.0.4", "Compatability: Windows, MacOS, Linux"))));
+        softwares.add(new Software("PowerPoint", "Program for presentation", 100000, "https://www.tcd.ie/CAPSL/TIC/assets/img/accessible-info/powerpoint.png", (Arrays.asList("Version: 14.0.4", "Compatability: Windows, MacOS, Linux"))));
+        softwares.add(new Software("WinRar", "Program for rar files", 0, "https://cdn.worldvectorlogo.com/logos/winrar-1.svg", (Arrays.asList("Version: 14.0.4", "Compatability: Windows, MacOS, Linux"))));
+        //softwares.add(new Software("Not real program", "Test program for very very very very very very very very very very very very very very very very very very very very very very very very long description", 0, "https://cdn3.vectorstock.com/i/1000x1000/19/77/isolated-abstract-blue-color-thumb-up-contour-logo-vector-10861977.jpg"));
+        
+        
         em.getTransaction().begin();
         for(Software s : softwares)
             em.persist(s);
