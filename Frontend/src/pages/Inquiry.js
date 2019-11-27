646d0f8c44c2b3fc8659b09109abb8bb2900e5aa
WSHuusfeldt <43413833+WSHuusfeldt@users.noreply.github.com>
2019-11-27 15:18:46 +0100

    Fixed ID for inquiries

--- Frontend/src/pages/Inquiry.js
+++ Frontend/src/pages/Inquiry.js
@@ -1,7 +1,7 @@
 import React, { useState, useEffect } from 'react';
-//import { Link } from 'react-router-dom';
+import { Link } from 'react-router-dom';
 import Facade from '../login/ApiFacade';
-//import settings from '../settings';
+import settings from '../settings';
 
 export default function Inquiries() {
   const [data, setData] = useState([]);
@@ -12,17 +12,26 @@ export default function Inquiries() {
 
   return (
     <div>
+        <h1>These are all the inquiries still open.</h1>
       <table className="table">
         <thead>
           <tr>
             <th scope="col">ID</th>
             <th scope="col">Name</th>
             <th scope="col">Subject</th>
-            <th scope="col">Phone</th>
+            <th scope="col">Email</th>
           </tr>
         </thead>
         <tbody>
-            
+            {data.map(inquiry =>
+                <tr key={inquiry.id}>
+                    <th scope="row">{inquiry.id}</th>
+                    <td>{inquiry.fullName}</td>
+                    <td>{inquiry.subject}</td>
+                    <td>{inquiry.email}</td>
+                    <td><Link to={settings.getURL("Inquiry") + "/" + (inquiry.id)} className="btn-zoid">Show details</Link></td>
+                </tr>
+                )}
         </tbody>
       </table>
     </div>
