4c055776e397468084d687ee1230cad1aa746dfb
AndreasVikke <vikkedesign@gmail.com>
2019-11-27 12:29:43 +0100

    Fixed Imports

--- Backend/src/test/java/facades/SoftwareFacadeTest.java
+++ Backend/src/test/java/facades/SoftwareFacadeTest.java
@@ -15,7 +15,6 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
-import org.junit.jupiter.api.Disabled;
 import utils.EMF_Creator;
 
 /**
